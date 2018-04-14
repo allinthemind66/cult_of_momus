064c946b22b6df024bcb8c4353c5e0b6dfc3b0b7
allinthemind66 <r.nilon92@gmail.com>
2018-04-14 16:57:29 -0400

    set up api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.create(title: "The Room", release_year: 2003, director: "Tommy Wiseau")
