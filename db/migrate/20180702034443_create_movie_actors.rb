5691529936afe480fe9798723c76103ccfc83902
allinthemind66 <r.nilon92@gmail.com>
2018-07-01 23:50:03 -0400

    create schema for actor and movie_actor

--- db/migrate/20180702034443_create_movie_actors.rb
+++ db/migrate/20180702034443_create_movie_actors.rb
@@ -1,7 +1,8 @@
 class CreateMovieActors < ActiveRecord::Migration[5.1]
   def change
     create_table :movie_actors do |t|
-
+      t.integer :movie_id
+      t.integer :actor_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415013125) do
+ActiveRecord::Schema.define(version: 20180702034443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movie_actors", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "actor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
